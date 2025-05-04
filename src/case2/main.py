391702d5b498950d53b93a35af3a1caeeb16a84c
zeljkoAntunovic01 <zeljko.ant123@gmail.com>
2025-05-04 14:39:02 +0200

    filtered out wrong scale values for emotion scale plots

--- src/case2/main.py
+++ src/case2/main.py
@@ -4,20 +4,20 @@ from decomposition.nmf import run_nmf_pipeline
 from decomposition.ica import run_ica_pipeline
 from decomposition.aa import run_aa_pipeline
 from decomposition.sc import run_sc_pipeline
-from som import som_pipeline
+from subspace_methods.som import som_pipeline
 
 def main():
     # Loading the data
     hr_df, X, y = run_data_loading()
-
+    """
     # Decomposition methods
     run_nmf_pipeline(X, y)
     run_ica_pipeline(X, y)
     run_aa_pipeline(X, y)
-    run_sc_pipeline(X, y)
+    run_sc_pipeline(X, y) """
     
     # Subspace methods
-    som_pipeline()
+    som_pipeline(X, y)
 
 
 if __name__ == "__main__":
